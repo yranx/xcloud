ff68545de885e76884397b13208462223dc6cf2d
ranx <yranforever@163.com>
2019-06-21 19:05:19 +0800

    feat: add SpringCloud Config + Eureka

---
+++ cloud-config-server-eureka/src/main/java/com/ranx/CloudConfigServerEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.ranx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudConfigServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerEurekaApplication.class, args);
+	}
+
+}
---
+++ cloud-eureka-provider4/src/main/java/com/ranx/CloudEurekaProviderApplication.java
@@ -0,0 +1,29 @@
+package com.ranx;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class CloudEurekaProviderApplication {
+
+	@Value("${server.port}")
+	String port;
+
+    @Value("${content}")
+    private String content;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaProviderApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world, port: " + port + ",content=" + content;
+	}
+}
